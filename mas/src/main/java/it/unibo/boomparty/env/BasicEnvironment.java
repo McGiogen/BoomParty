3c55af82635e6a4cf7bc1bf43ec384cbef2c99ce
luca.maestri <luca.maestri@studuio.unibo.it>
2018-10-10 21:47:51 +0200

    Creato environment con primo esempio di avvio server.

---
+++ mas/src/main/java/it/unibo/boomparty/env/BasicEnvironment.java
@@ -0,0 +1,13 @@
+package it.unibo.boomparty.env;
+
+import it.unibo.boomparty.Main;
+import jason.environment.Environment;
+
+public class BasicEnvironment extends Environment{
+
+	@Override
+	public void init(final String[] args) {
+		System.out.println("abebe");
+		Main.main(null);
+	}
+}
