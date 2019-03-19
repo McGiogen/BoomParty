4f59ac259be4145a32eef9dcd599b45d909a4034
McGiogen <mcgiogen@hotmail.it>
2019-03-20 00:58:50 +0100

    Stampa i vincitori grigi e aggiunge uno sfondo

--- src/main/java/it/unibo/boomparty/env/WorldView.java
+++ src/main/java/it/unibo/boomparty/env/WorldView.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.FontMetrics;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import it.unibo.boomparty.constants.GameConstans.TEAM_PLAYER;
 import it.unibo.boomparty.constants.GameConstans.ROLE_PLAYER;
@@ -49,13 +51,31 @@ public class WorldView extends GridWorldView {
     }
 
     public void drawVincitori(final Graphics g, final int x, final int y) {
-        // TODO Stampa anche i grigi
-        // TODO Migliora la stampa
-        String s = "Vincono i " + (this.model.squadraVincitrice.equals(TEAM_PLAYER.ROSSO) ? "Rossi" : "Blue") + "!!!";
+        // Stampa team vincitore
+        String s1 = "Vincono i " + (this.model.squadraVincitrice.equals(TEAM_PLAYER.ROSSO) ? "Rossi" : "Blue") + "!!!";
+
+        // Stampa vincitori grigi
+        List<String> grigiVincitori = this.model.grigiVincitori.stream()
+                .map(ROLE_PLAYER::getValue)
+                .collect(Collectors.toList());
+        String s2 = grigiVincitori.size() > 0 ? "Vincitori grigi: " + String.join(", ", grigiVincitori) : "Nessun vincitore grigio";
 
         g.setFont(this.defaultFont);
         FontMetrics metrics = g.getFontMetrics();
         int height = metrics.getHeight();
-        g.drawString( s, cellSizeW/2, cellSizeH/2 + height/2);
+        int width1 = metrics.stringWidth( s1 );
+        int width2 = metrics.stringWidth( s2 );
+
+        // Disegno gli sfondi
+        g.setColor(this.model.squadraVincitrice.equals(TEAM_PLAYER.ROSSO) ? Color.red : Color.blue);
+        g.fillRect(cellSizeW/2 - 2,cellSizeH/2 - 6, width1 + 4, height + 2);
+
+        g.setColor(Color.gray);
+        g.fillRect(cellSizeW/2 - 2, height + cellSizeH/2 - 4, width2 + 4, height + 2);
+
+        // Stampo i testi
+        g.setColor(Color.white);
+        g.drawString(s1, cellSizeW/2, cellSizeH/2 + height/2);
+        g.drawString(s2, cellSizeW/2, height + 2 + cellSizeH/2 + height/2);
     }
 }
