879c5e8c68b5da610360868078a0987ca727aa92
Alessandro Neri <alessandro.neri90@gmail.com>
2019-05-21 22:17:30 +0200

    Fix debug options

--- src/main/java/it/unibo/boomparty/Main.java
+++ src/main/java/it/unibo/boomparty/Main.java
@@ -19,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
 
         SimulationArgs simArgs = new SimulationArgs();
-        simArgs.setDebug(false);
+        simArgs.setDebug(true);
         simArgs.setPlayers(13);
 
         Map<TEAM, List<ROLE>> teamRuoliMap = new HashMap<>();
